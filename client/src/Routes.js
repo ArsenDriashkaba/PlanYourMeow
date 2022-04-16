47fcc8c3fb1ff67418957e46b8e6475d60c75042
ArsenDriashkaba <arsen.dryashkaba.super@gmail.com>
2022-04-17 00:53:56 +0200

    F5: Creating basic template of TicketDetailPage

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,6 +2,7 @@ import { Routes as RouterRoutes, Route } from "react-router-dom";
 import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
 import WorkspaceListPage from "./pages/WorkspaceListPage/WorkspaceListPage";
 import WorkspaceDetailPage from "./pages/WorkspaceDetailPage/WorkspaceDetailPage";
+import TicketDetailPage from "./pages/TicketDetailPage/TicketDetailPage";
 import MainPage from "./pages/MainPage/MainPage";
 
 export function Routes() {
@@ -10,6 +11,7 @@ export function Routes() {
       <Route path="/" element={<MainPage />} />
       <Route path="/workspaces" element={<WorkspaceListPage />} />
       <Route path="/workspaces/:id" element={<WorkspaceDetailPage />} />
+      <Route path="/tickets/:id" element={<TicketDetailPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </RouterRoutes>
   );
---
+++ client/src/pages/TicketDetailPage/TicketDetailPage.js
@@ -0,0 +1,65 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import api from "../../Api";
+
+import "./TicketDetailPage.css";
+
+const TicketDetailPage = () => {
+  const { id } = useParams();
+  const [ticketInfo, setTicketInfo] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const setFetchedTicketInfo = () => {
+    api
+      .get(`/tickets/${id}`)
+      .then((res) => setTicketInfo(res.data))
+      .catch((error) => setError(error));
+
+    console.log("Ticket info is fetched!");
+  };
+
+  const toggleEditMode = () => {
+    editMode ? setEditMode(false) : setEditMode(true);
+  };
+
+  useEffect(setFetchedTicketInfo, [id]);
+
+  if (isLoading) {
+    return <p>Is loading</p>;
+  }
+
+  if (error) {
+    return <p>Error occured :c</p>;
+  }
+
+  const { name, description, deadline, label, state, userId } = {
+    ...ticketInfo,
+  };
+
+  return (
+    <section id="ticket-detail-page">
+      <header id="ticket-page-header">
+        <h1>{name}</h1>
+        {editMode ? (
+          <button onClick={toggleEditMode}>Save</button>
+        ) : (
+          <button onClick={toggleEditMode}>Edit</button>
+        )}
+      </header>
+      <div id="ticket-details">
+        <p>{description}</p>
+      </div>
+      <div id="ticket-params-container">
+        <span>{userId ? userId : "Assign user"}</span>
+        <span>{label ? label : "Choose label"}</span>
+        <span>{deadline ? deadline : "Choose deadline"}</span>
+        <span>{state ? state : "State of the ticket"}</span>
+      </div>
+    </section>
+  );
+};
+
+export default TicketDetailPage;
