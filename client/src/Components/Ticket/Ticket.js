2975a08bf57560b7beb12860b0a5a2b08fb29d81
ArsenDriashkaba <83950642+ArsenDriashkaba@users.noreply.github.com>
2022-04-17 21:04:57 +0200

    Features 5/ticket detail edit page (#5)
    * F5: Creating basic template of TicketDetailPage
    
    * F5: Creating routes for editing and getting ticket by ID
    
    * F5: Implementation basic version of page for updating ticket info + basic styling
    
    * Report 5

--- client/src/Components/Ticket/Ticket.js
+++ client/src/Components/Ticket/Ticket.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import DeleteElementBtn from "../DeleteElementBtn/DeleteElementBtn";
+import { Link } from "react-router-dom";
 
 import "./Ticket.css";
 
@@ -15,7 +16,9 @@ const Ticket = ({ name, ticket_id, fetchData }) => {
     <div className="ticket">
       <div className="ticket-info-container">
         <header className="ticket-header">
-          <h2 className="ticket-name">{name}</h2>
+          <Link to={`/tickets/${ticket_id}`}>
+            <h2 className="ticket-name">{name}</h2>
+          </Link>
         </header>
         <div className="ticket-person"></div>
       </div>
---
+++ client/src/pages/TicketDetailPage/TicketDetailPage.js
@@ -0,0 +1,84 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import TicketPageHeader from "../../Components/Ticket/TicketPageHeader/TicketPageHeader";
+import TicketParamsContainer from "../../Components/Ticket/TicketParamsContainer/TicketParamsContainer";
+import TicketDescription from "../../Components/Ticket/TicketDescription/TicketDescription";
+import api from "../../Api";
+
+import "./TicketDetailPage.css";
+
+const TicketDetailPage = () => {
+  const { id } = useParams();
+  const [ticketInfo, setTicketInfo] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [newTicketParams, setNewTicketParams] = useState();
+  const [error, setError] = useState();
+
+  const setFetchedTicketInfo = () => {
+    setLoading(true);
+
+    api
+      .get(`/tickets/${id}`)
+      .then((res) => setTicketInfo(res.data))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+
+    console.log("Ticket info is fetched!");
+  };
+
+  const updateTicketInfo = () => {
+    api
+      .patch(`/tickets/${id}`, newTicketParams)
+      .then(() => {
+        setFetchedTicketInfo();
+        console.log("Ticket is updated :)");
+      })
+      .catch((error) => setError(error));
+  };
+
+  const toggleEditMode = () => {
+    editMode ? setEditMode(false) : setEditMode(true);
+  };
+
+  useEffect(setFetchedTicketInfo, [id]);
+
+  if (isLoading) {
+    return <p>Is loading</p>;
+  }
+
+  if (error) {
+    return <p>Error occured :c</p>;
+  }
+
+  const { name, description } = {
+    ...ticketInfo,
+  };
+
+  return (
+    <section id="ticket-detail-page">
+      <TicketPageHeader
+        name={name}
+        editMode={editMode}
+        toggleEditMode={toggleEditMode}
+        fetchData={setFetchedTicketInfo}
+        setTicketParams={setNewTicketParams}
+        ticketInfo={ticketInfo}
+        updateTicketInfo={updateTicketInfo}
+      />
+      <div className="ticket-detail-body">
+        <TicketDescription
+          description={description}
+          editMode={editMode}
+          fetchData={setFetchedTicketInfo}
+          setTicketParams={setNewTicketParams}
+          ticketInfo={ticketInfo}
+        />
+        <TicketParamsContainer />
+      </div>
+    </section>
+  );
+};
+
+export default TicketDetailPage;
