84fcd760b4dd7a445d2ff8e73d47e01307443ca3
ArsenDriashkaba <arsen.dryashkaba.super@gmail.com>
2022-04-24 21:00:45 +0200

    F6: Creating LoginForm and RegistrationForm components

---
+++ client/src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,31 @@
+import "./LoginForm.css";
+
+const LoginForm = () => {
+  return (
+    <form>
+      <label>Username : </label>
+      <input
+        type="text"
+        placeholder="Enter Username"
+        name="username"
+        required
+      />
+      <label>Password : </label>
+      <input
+        type="password"
+        placeholder="Enter Password"
+        name="password"
+        required
+      />
+      <button type="submit">Login</button>
+      <input type="checkbox" checked="checked" /> Remember me
+      <button type="button" class="cancelbtn">
+        {" "}
+        Cancel
+      </button>
+      Forgot <span> password? </span>
+    </form>
+  );
+};
+
+export default LoginForm;
