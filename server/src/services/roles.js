d2986fc8f529665b58be95dc15ebc34a848ab8d9
ArsenDriashkaba <arsen.dryashkaba.super@gmail.com>
2022-05-01 18:15:28 +0200

    F7: Implementation/Refactoring of Role routes

--- server/src/services/roles.js
+++ server/src/services/roles.js
@@ -6,51 +6,125 @@ const addRole = async (req, res) => {
     const validationResults = validationResult(req);
 
     if (validationResults.isEmpty()) {
-      const name = req.body.name;
+      const newRole = await sequelize.models.role.create({
+        name: req.body.name,
+      });
 
-      database.query(
-        `INSERT INTO roles (name) VALUES (?)`,
-        [name],
-        (err, result) => {
-          if (err) {
-            console.log(err);
-          } else {
-            res.status(200).send(result);
-          }
-        }
-      );
+      res.status(200).send(newRole);
     } else {
       req.log.info(`Validation error value: ${validationResults}`);
       res.status(400).send(validationResults);
     }
   } catch (error) {
-    req.log.error(error);
+    console.log(error);
     res.status(500).send("Error!");
   }
 };
 
 const getAllRoles = async (req, res) => {
   try {
-    req.log.info("Success");
-    res.status(200).send("Hello");
+    const roles = await req.context.models.role.findAll();
+
+    res.status(200).send(roles);
   } catch (error) {
-    req.log.error(error);
+    console.log(error);
     res.sendStatus(500);
   }
 };
 
-const getRoleById = async (req, res) => {};
+const getRoleById = async (req, res) => {
+  try {
+    const validationResults = validationResult(req);
+    const models = req.context.models;
+    const roleModel = models.role;
 
-const editRoleById = async (req, res) => {};
+    if (validationResults.isEmpty()) {
+      const role = await roleModel.findOne({
+        where: { id: req.params.id },
+      });
+
+      res.status(200).send(role);
+    } else {
+      res.send(400);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
+
+const editRoleById = async (req, res) => {
+  try {
+    const validationResults = validationResult(req);
+
+    if (!validationResults.isEmpty()) {
+      console.log(validationResults);
+      res.status(400).send(validationResults);
+
+      return;
+    }
+
+    const roleModel = await sequelize.models.role;
+    const roleId = req.params.id;
+    const role = await roleModel.findOne({
+      where: { id: roleId },
+    });
+
+    if (role == null) {
+      const notFoundMsg = `Role with id "${roleId}" is not found...`;
+
+      console.log(notFoundMsg);
+      res.status(404).send(notFoundMsg);
+
+      return;
+    }
+
+    await roleModel.update(req.body, { where: { id: roleId } });
+
+    const successMsg = `You've succsesfully updated role with id: ${req.params.id}`;
+
+    console.log(successMsg);
+    res.status(200).send(successMsg);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+};
 
 const deleteRoleById = async (req, res) => {
   try {
+    const validationResults = validationResult(req);
+
+    if (!validationResults.isEmpty()) {
+      console.log(validationResults);
+      res.status(400).send(validationResults);
+
+      return;
+    }
+
+    const roleId = req.params.id;
+    const role = await req.context.models.role.findOne({
+      where: { id: roleId },
+    });
+
+    if (role == null) {
+      const notFoundMsg = `Role with id "${roleId}" is not found...`;
+
+      res.status(404).send(notFoundMsg);
+
+      return;
+    }
+
+    await sequelize.models.role.destroy({
+      where: { id: roleId },
+    });
+
     const successMsg = `You've succsesfully deleted role with id: ${req.params.id}`;
 
-    req.log.info(successMsg);
+    console.log(successMsg);
     res.status(200).send(successMsg);
   } catch (error) {
-    req.log.error(error);
+    console.log(error);
     res.sendStatus(500);
   }
 };
